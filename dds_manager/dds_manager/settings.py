402da39f4779dde34f1c6d9ce1031d3972af714c
cyrilcon <constantine.sonic@gmail.com>
2025-07-28 18:32:54 +0500

    feat: init cashflow and catalogs apps

--- dds_manager/dds_manager/settings.py
+++ dds_manager/dds_manager/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "cashflow.apps.CashflowConfig",
+    "catalogs.apps.CatalogsConfig",
 ]
 
 MIDDLEWARE = [
@@ -112,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
